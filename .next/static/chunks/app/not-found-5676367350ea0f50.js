30c9fc0f71331f4910b0df912e51e321d23d8ee7
Aminaliipour <aminemalipour@gmail.com>
2025-06-16 13:44:02 +0330

    Fixed Vercel Problems

---
+++ .next/static/chunks/app/not-found-5676367350ea0f50.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{8474:function(n,e,u){Promise.resolve().then(u.t.bind(u,2216,23))}},function(n){n.O(0,[216,44,550,744],function(){return n(n.s=8474)}),_N_E=n.O()}]);
